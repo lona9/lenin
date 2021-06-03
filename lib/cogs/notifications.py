b10461548707bd0f8e5f4a94aba4ee09c55e0e7d
lona9 <pilar.vasquez.h@gmail.com>
2021-06-03 17:14:32 -0400

    update notifications messages

--- lib/cogs/notifications.py
+++ lib/cogs/notifications.py
@@ -9,11 +9,19 @@ class Notifications(Cog):
   def __init__(self, bot):
     self.bot = bot
 
+  ## INFOGRAFIA ANIVERSARIO
+  @command(name="anni", aliases=["aniversario"])
+  async def anni(self, ctx):
+    await ctx.message.delete()
+    await ctx.channel.send("Camaradas, hemos recopilado distintas estadísticas de nuestro server a lo largo de este año que queremos compartir con todes, estamos felices de estar a días de cumplir un año desde la primera vez que nos juntamos a discutir de política! Les compartimos una infografía de los números más relevantes del server, así como un word cloud construido con todos los mensajes del canal <#716081744390127717>.\nNo olvidar que este sábado 5 de junio nos juntamos a las 14:00 (GMT -4, hora chilena) para hablar sobre **Kpop y marxismo** y celebrar nuestro primer aniversario!\n<@&716089943130243092>")
+    await ctx.channel.send(file=discord.File(os.path.join("/root/lenin/data/img/", "estadisticas.png")))
+    await ctx.channel.send(file=discord.File(os.path.join("/root/lenin/data/img/", "orbitburo.png")))
+
   ## MENSAJE SERVER
   @command(name="actividad", aliases=["act"])
   async def actividad(self, ctx):
     await ctx.message.delete()
-    await ctx.channel.send("Para celebrar un año desde que empezamos las actividades de este servidor, invitamos a todes a participar de una actividad este sábado 5 de junio a las 14:00 horas (GMT -4, hora chilena), donde discutiremos sobre **Kpop y marxismo**, en conmemoración a los dos grandes temas que engloban a OrbitBuró en Español.\n¡Les esperamos este sábado para la celebración!\n<@&801508398662942790>")
+    await ctx.channel.send("Les recordamos este sábado 5 de junio a las 14:00 horas (GMT -4, hora chilena) tendremos nuestra actividad de aniversario, donde discutiremos sobre **Kpop y marxismo**, en conmemoración a los dos grandes temas que engloban a OrbitBuró en Español.\n¡Les esperamos este sábado para la celebración!\n<@&801508398662942790>")
     await ctx.channel.send(file=discord.File(os.path.join("/root/lenin/data/img/", "aniversario_orbitburo.png")))
 
   #DM NOTIFICACIONES
@@ -28,7 +36,7 @@ class Notifications(Cog):
     for member in guild.members:
       if role_id in member.roles:
           try:
-              await member.send("Para celebrar un año desde que empezamos las actividades de este servidor, invitamos a todes a participar de una actividad este sábado 5 de junio a las 14:00 horas (GMT -4, hora chilena), donde discutiremos sobre **Kpop y marxismo**, en conmemoración a los dos grandes temas que engloban a OrbitBuró en Español.\n¡Les esperamos este sábado para la celebración!")
+              await member.send("Les recordamos este sábado 5 de junio a las 14:00 horas (GMT -4, hora chilena) tendremos nuestra actividad de aniversario, donde discutiremos sobre **Kpop y marxismo**, en conmemoración a los dos grandes temas que engloban a OrbitBuró en Español.\n¡Les esperamos este sábado para la celebración!")
               await member.send(file=discord.File(os.path.join("/root/lenin/data/img/", "aniversario_orbitburo.png")))
           except Forbidden:
               pass
